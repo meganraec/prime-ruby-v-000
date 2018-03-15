f02fb7dbaf64fe777baff6f9cb6d8438b2e5918c
Megan Collier <meganraec@live.com>
2018-03-15 17:49:57 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(n)
+  if n < 2
+    return false
+  elsif n == 2 || n == 3
+    return true
+  elsif n >= 4
+    a = (2..n-1).to_a
+    a.each do |x|
+      # while (x * x <= n)
+      n % x == 0
+      return false
+      end
+  else
+    return true
+  end
+end
