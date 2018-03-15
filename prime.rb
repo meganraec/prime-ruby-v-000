8d2ab8f9bd4923131b59ed94c41dfdd2a92a9d03
Megan Collier <meganraec@live.com>
2018-03-15 19:23:33 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -6,17 +6,26 @@ def prime?(n)
   elsif n >= 4 && n % 2 == 0
     return  false
   elsif n >= 4
-    x = (2..n-1).to_a
-    x.each do |i|
-      while (i * i <= n) do
-      n % i == 0
-      break if false
+    x = 2
+    while (x * x <= n)
+      if n % x == 0
+        return false
       end
+    x += 1
     end
-  else
-    return true
-  end
+  return true
 end
+#     x = (2..n-1).to_a
+#     x.each do |i|
+#       while (i * i <= n) do
+#       n % i == 0
+#       break if false
+#       end
+#     end
+#   else
+#     return true
+#   end
+# end
 
 
 # for i in (2..n-1)
