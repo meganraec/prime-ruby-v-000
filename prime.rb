7ac4546938faa7e84937d0ebfc78a4814f85c3fa
Megan Collier <meganraec@live.com>
2018-03-15 18:20:01 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -4,20 +4,13 @@ def prime?(n)
   elsif n == 2 || n == 3
     return true
   elsif n >= 4
-    for i in (2..n-1)
-      n % i == 0
-      return false
+    i = 2
+    unless n % i == 0
+      for i in (2..n-1)
+      i += 1
+    return false
     end
   else
     return true
   end
 end
-
-
-# elsif n >= 4
-#   i = 2
-#     while (i * i <= n) do
-#     n % i == 0
-#     return false
-#     i += 1
-#     end
